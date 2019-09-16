8b0eaf0fec84bb01d7e6f9ca757769e56e44c3c7
Положиев Роман <(roma.polozhiev@gmail.com)>
2019-09-16 11:34:19 +0300

    РАБОТАЕТ!

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -8,8 +8,44 @@ import java.util.List;
  * Created by artemaliev on 07/09/15.
  */
 public class IntSort {
-  public static void sort (int array[]) {
-    Arrays.sort(array);
+  public static void quickSort(int[] array, int low, int high) {
+    if (array.length == 0)
+      return;
+
+    if (low >= high)
+      return;
+
+    int middle = low + (high - low) / 2;
+    int opora = array[middle];
+
+    int i = low, j = high;
+    while (i <= j) {
+      while (array[i] < opora) {
+        i++;
+      }
+
+      while (array[j] > opora) {
+        j--;
+      }
+
+      if (i <= j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        i++;
+        j--;
+      }
+    }
+
+    if (low < j)
+      quickSort(array, low, j);
+
+    if (high > i)
+      quickSort(array, i, high);
+  }
+
+  public static void sort (int[] array) {
+      quickSort(array, 0, array.length - 1);
   }
 
   public static void sort (List<Integer> list) {
