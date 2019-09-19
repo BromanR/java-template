5ae7120fb069edc8653ab9c14536a47895da8425
BromanR <roma.polozhiev@gmail.com>
2019-09-19 10:35:53 +0300

    Смотрю как работает commit

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -20,13 +20,9 @@ public class IntSort {
 
     int i = low, j = high;
     while (i <= j) {
-      while (array[i] < opora) {
-        i++;
-      }
+      while (array[i] < opora) i++;
 
-      while (array[j] > opora) {
-        j--;
-      }
+      while (array[j] > opora) j--;
 
       if (i <= j) {
         int temp = array[i];
