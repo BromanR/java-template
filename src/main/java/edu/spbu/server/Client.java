90074f3e82f77fccb0cad668b4ca74ad7118d465
BromanR <roma.polozhiev@gmail.com>
2019-12-19 12:28:24 +0300

    18.12 client-server v 1.0

--- src/main/java/edu/spbu/server/Client.java
+++ src/main/java/edu/spbu/server/Client.java
@@ -3,37 +3,63 @@ package edu.spbu.server;
 import java.io.*;
 import java.net.*;
 
-public class Client {
-  static private Socket clientSocket;
-  private static BufferedReader reader; // нам нужен ридер читающий с консоли
-  private static BufferedReader in; // поток чтения из сокета
-  private static BufferedWriter out; // поток записи в сокет
+public class Client implements Runnable {
 
+  static private Socket connection;
+  static private OutputStream output;
+  static private InputStream input;
 
-  public static void main(String[] args) throws IOException {
-    try {
-      while (true) {
-        clientSocket = new Socket("localhost", 8080);
-        reader = new BufferedReader(new InputStreamReader(System.in));
-        // читать соообщения с сервера
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        // писать туда же
-        out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-        System.out.println("Вы что-то хотели сказать? Введите это здесь:");
-        String word = reader.readLine(); // ждём пока клиент что-нибудь
-        // не напишет в консоль
-        if (word.equals("quit")) break;
-        out.write(word + "\n"); // отправляем сообщение на сервер
-        out.flush();
-        String serverWord = in.readLine(); // ждём, что скажет сервер
-        System.out.println(serverWord); // получив - выводим на экран
-      }
-      System.out.println("Клиент был закрыт...");
-      clientSocket.close();
-      in.close();
-      out.close();
-    } catch (IOException e) {
-      System.err.println(e);
+  public static void main(String[] args) {
+    try{
+      Client client = new Client("www.rzd.ru", 80);
+//      Client client = new Client("127.0.0.1", 8080);
+      client.sendData("Server");
+      client.receiveData();
+    } catch(IOException e){
+      e.printStackTrace();
     }
   }
+
+  //конструктор клиента
+  public Client(String serverName, int port ) throws IOException {
+    connection = new Socket(InetAddress.getByName(serverName),port);
+
+    output = connection.getOutputStream();
+    System.out.println("DataOutputStream  created");
+
+    input = connection.getInputStream();
+    System.out.println("DataInputStream created");
+  }
+
+  @Override
+  public void run(){
+
+  }
+
+  //посылает запрос
+  private void sendData(String serverName) throws IOException {
+//    String s = "GET /test.html HTTP/1.1\r\nHost:" + serverName +"\r\n\r\n";
+    String s = "GET / HTTP/1.1\r\nHost:" + serverName +"\r\n\r\n";
+    output.write(s.getBytes());
+    output.flush();
+    System.out.println("Запрос отправлен");
+  }
+
+  //получает ответ
+  private void receiveData() {
+    try{
+      BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+      System.out.println("Работаем...\n");
+      String s = reader.readLine();
+      if(s!=null)
+        System.out.println("Ответ: ");
+      while(s!=null){
+        System.out.println(s);
+        s=reader.readLine();
+      }
+    } catch(IOException e){
+      e.printStackTrace();
+    }
+  }
+
 }
--- src/main/java/edu/spbu/server/Server.java
+++ src/main/java/edu/spbu/server/Server.java
@@ -3,31 +3,99 @@ package edu.spbu.server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Server {
 
-  static private Socket clientSocket; //сокет для общения
-  private static BufferedReader in; // поток чтения из сокета
-  private static BufferedWriter out; // поток записи в сокет
+  static private Socket connection;
+  static private OutputStream output;
+  static private InputStream input;
 
   public static void main(String[] args) {
     try {
       int port = 8080;
-      ServerSocket server = new ServerSocket(port); //северсокет прослушивает порт 8080
+      ServerSocket server = new ServerSocket(port);
       System.out.println("Waiting for connection...");
       while (true) {
-        clientSocket = server.accept();  // становимся в ожидание подключения к сокету под именем - "clientSocket" на серверной стороне
+        connection = server.accept();
         System.out.println("Connection accepted.");
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-        String word = in.readLine(); // ждём пока клиент что-нибудь нам напишет
-        System.out.println(word);
-        out.write("Привет, это Сервер! Подтверждаю, вы написали : " + word + "\n");
-        out.flush(); // выталкиваем все из буфера
+
+        output = connection.getOutputStream();
+        System.out.println("DataOutputStream  created");
+
+        input = connection.getInputStream();
+        System.out.println("DataInputStream created");
+
+        String filePath = receiveData(); //получает запрос клиента
+        sendData(filePath);              // отправляет ответ клиенту
+        server.close();
       }
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
+  //отправляет ответ
+  private static void sendData(String filePath) throws IOException {
+    File file = new File(filePath);
+    if (file.exists()) {
+      try (FileReader fileRead = new FileReader(file)) {
+
+        output.flush();
+        BufferedReader reader = new BufferedReader(fileRead);
+        StringBuilder text = new StringBuilder();
+        String i = reader.readLine();
+
+        //чтение из файла
+        while (i != null) {
+          text.append(i);
+          i = reader.readLine();
+        }
+
+        String content = text.toString();
+        String message = "HTTP/1.1 200 OK\r\n" +
+            "Server: ServerName\r\n" +
+            "Content-Type: text/html\r\n" +
+            "Connection: close\r\n\r\n" + content;
+        output.write(message.getBytes());
+        System.out.println("Ответ отправлен");
+        output.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    } else {
+      output.write("HTTP/1.1 404\r\n\r\n".getBytes());
+    }
+
+  }
+
+  //получает запрос
+  private static String receiveData() {
+
+    String filePath;
+    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+    System.out.println("Запрос принят");
+    try {
+      String line;
+      if ((line = reader.readLine()) != null) {
+     //   System.out.println(line);
+        String[] st = line.split(" ");
+        System.out.println(Arrays.toString(st));
+        if (st[1].length() > 0) {
+          filePath = st[1].substring(1);
+          System.out.println("Запрос принят. Ищем файл: " + filePath);
+          //печать запроса клиента
+//                    String str;
+//                    while((str=reader.readLine()).length()!=0)
+//                        System.out.println(str);
+
+          return filePath;
+        }
+      } else System.out.println("Где-то ошибка");
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return "Empty";
+  }
 }
